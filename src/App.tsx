3817604cef2e633ac30d6350202a88db01fc9347
VadimKorobec <_s_e_t__@ukr.net>
2024-02-12 14:46:39 +0200

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,31 @@ import { nanoid } from "nanoid";
 import "./App.css";
 import { TaskType, TodoList } from "./components/TodoList";
 import todos from "./components/todos.json";
+import { Input } from "./components/Input";
 
-export type FilterValuesType = 'all' | 'active' | 'completed';
+export type FilterValuesType = "all" | "active" | "completed";
 
 export const App = () => {
   const [tasks, setTasks] = useState<TaskType[]>(todos);
   const [filter, setFilter] = useState<FilterValuesType>("all");
 
-  const handleAddTask = (title:string) => {
+  const handleAddTask = (title: string) => {
     // let newTask = { id: nanoid(), title: "New Task", isDone: false }
     // let newTasks = [...tasks, newTask]
     // setTasks(newTasks)
-    setTasks(prevState => [...prevState,{id:nanoid(),title, isDone:false}])
-  }
+    setTasks((prevState) => [
+      ...prevState,
+      { id: nanoid(), title, isDone: false },
+    ]);
+  };
 
   const handleDeleteTask = (id: string) => {
     setTasks((prevState) => prevState.filter((item) => item.id !== id));
   };
 
-  const handleFilterTasks = (value:FilterValuesType) => {
-    setFilter(value)
-  }
+  const handleFilterTasks = (value: FilterValuesType) => {
+    setFilter(value);
+  };
 
   let tasksForTodoList = tasks;
   if (filter === "completed") {
@@ -40,8 +44,9 @@ export const App = () => {
         tasks={tasksForTodoList}
         onDelete={handleDeleteTask}
         onFilter={handleFilterTasks}
-        addTask ={handleAddTask}
+        addTask={handleAddTask}
       />
+      <Input />
     </div>
   );
 };
