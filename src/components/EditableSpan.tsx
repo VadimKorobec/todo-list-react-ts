cac7bfd590bff4832edb0e069dff015e818e1040
Vadim <korobecv2@gmail.com>
2024-02-16 16:55:35 +0200

    Update EditableSpan.tsx

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -2,28 +2,37 @@ import { useState } from "react";
 
 type EditableSpanPropsType = {
   title: string;
+  onChange: (newValue:string) => void;
 };
 
-export const EditableSpan = ({ title }: EditableSpanPropsType) => {
+export const EditableSpan = ({ title,onChange }: EditableSpanPropsType) => {
   const [show, setShow] = useState<boolean>(false);
-  const [newTitle, setNewTitle] = useState<string>('')
-  
-  const handleChange = (e:React.ChangeEvent<HTMLInputElement>) => {
-    setNewTitle(e.currentTarget.value)
-  }
+  const [newTitle, setNewTitle] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTitle(e.currentTarget.value);
+  };
 
   const handleChangeTitle = () => {
     setShow(true);
   };
 
   const handleViewClick = () => {
-   setShow(false)
-  }
+    setShow(false);
+    onChange(newTitle)
+    setNewTitle('')
+  };
 
   return (
     <>
       {show ? (
-        <input type="text"  value={newTitle} onBlur={handleViewClick} autoFocus onChange={handleChange} />
+        <input
+          type="text"
+          value={newTitle}
+          onBlur={handleViewClick}
+          autoFocus
+          onChange={handleChange}
+        />
       ) : (
         <span onDoubleClick={handleChangeTitle}>{title}</span>
       )}
