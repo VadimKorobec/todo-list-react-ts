f4da44cce2593765e03d5a191b216976f3e3f3fb
Vadim <korobecv2@gmail.com>
2024-02-16 15:55:42 +0200

    Update TodoList.tsx

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -52,7 +52,7 @@ export const TodoList = ({
               checked={isDone}
               onChange={() => onStatus(id, isDone, id)}
             />
-            <EditableSpan title={title} editMode ={true} />
+            <EditableSpan title={title} />
             <button onClick={() => onDelete(id, id)}>Delete</button>
           </li>
         ))}
