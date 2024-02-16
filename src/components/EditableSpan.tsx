cd5186b0ffeb9f37b881c360b505800747b560ac
Vadim <korobecv2@gmail.com>
2024-02-16 15:36:56 +0200

    Update EditableSpan.tsx

--- src/components/EditableSpan.tsx
+++ src/components/EditableSpan.tsx
@@ -2,13 +2,17 @@ import { useState } from "react";
 
 type EditableSpanPropsType = {
   title: string;
+  editMode: boolean;
 };
 
 export const EditableSpan = ({ title }: EditableSpanPropsType) => {
   const [show, setShow] = useState<boolean>(false);
   return (
     <>
-      {!show ? <span onClick={() => setShow(show)}>{title}</span> : <input />}
+      {show ? <input /> : <span>{title}</span>}
+      <button type="button" onClick={() => setShow(!show)}>
+        Change Title
+      </button>
     </>
   );
 };
